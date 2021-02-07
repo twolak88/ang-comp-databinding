77d5627a6ba0e79bb417fdf87c2fd610b506cd39
twolak88 <twolak88@gmail.com>
2021-02-07 21:53:39 +0100

    Getting Access to ng-content with @ContentChild

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,4 @@
-import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, ElementRef, Input, OnChanges, OnDestroy, OnInit, SimpleChanges, ViewChild, ViewEncapsulation } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, ContentChild, DoCheck, ElementRef, Input, OnChanges, OnDestroy, OnInit, SimpleChanges, ViewChild, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -19,6 +19,7 @@ export class ServerElementComponent implements
   element: {type: string, name: string, content: string};
   @Input() name: string;
   @ViewChild('heading', {static: true}) header: ElementRef;
+  @ContentChild('contentParagraph', {static: true}) paragraph: ElementRef;
 
   constructor() {
     console.log('constructor');
@@ -28,40 +29,48 @@ export class ServerElementComponent implements
     console.log('ngOnChanges');
     console.log(changes);
     console.log('Text Content: ' + this.header.nativeElement.textContent);
+    console.log('Content Paragraph: ' + this.paragraph.nativeElement.textContent);
   }
 
   ngOnInit(): void {
     console.log('ngOnInit');
     console.log('Text Content: ' + this.header.nativeElement.textContent);
+    console.log('Content Paragraph: ' + this.paragraph.nativeElement.textContent);
   }
 
   ngDoCheck(): void {
     console.log('ngDoCheck');
     console.log('Text Content: ' + this.header.nativeElement.textContent);
+    console.log('Content Paragraph: ' + this.paragraph.nativeElement.textContent);
   }
 
   ngAfterContentInit(): void {
     console.log('ngAfterContentInit');
     console.log('Text Content: ' + this.header.nativeElement.textContent);
+    console.log('Content Paragraph: ' + this.paragraph.nativeElement.textContent);
   }
 
   ngAfterContentChecked(): void {
     console.log('ngAfterContentChecked');
     console.log('Text Content: ' + this.header.nativeElement.textContent);
+    console.log('Content Paragraph: ' + this.paragraph.nativeElement.textContent);
   }
 
   ngAfterViewChecked(): void {
     console.log('ngAfterViewChecked');
     console.log('Text Content: ' + this.header.nativeElement.textContent);
+    console.log('Content Paragraph: ' + this.paragraph.nativeElement.textContent);
   }
 
   ngAfterViewInit(): void {
     console.log('ngAfterViewInit');
     console.log('Text Content: ' + this.header.nativeElement.textContent);
+    console.log('Content Paragraph: ' + this.paragraph.nativeElement.textContent);
   }
 
   ngOnDestroy(): void {
     console.log('ngOnDestroy');
     console.log('Text Content: ' + this.header.nativeElement.textContent);
+    console.log('Content Paragraph: ' + this.paragraph.nativeElement.textContent);
   }
 }
